367ef967881446117e94037298c5adbe94c289f0
Женя Вито z z z <evgenizef.ai@gmail.com>
2021-12-12 01:03:09 +1000

    Добавьте файлы проекта.

---
+++ HoumWork23/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HoumWork23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
+//Создать анонимный тип икатрежи
+//ValueTupl Использовать в качестве агрумента метода и в качестве значения 
